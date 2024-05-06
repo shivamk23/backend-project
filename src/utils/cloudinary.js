f42c8d247bad47310e6d1ce74fd05430401505a0
shivamk23 <shivamdeveloper23@gmail.com>
2024-05-06 13:19:47 +0530

    added file and image upload properties

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,42 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    //upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    //file has been upload successfully
+    console.log("File uploaded successfully");
+    console.log(response.url);
+    return response;
+
+    //remove the file from the local storage
+    // fs.unlink
+
+  } catch (error) {
+    fs.unlinkSync(localFilePath); //remove the file from the locally saved temporary file as the upload opertaion got faild
+    return null;
+  }
+};
+
+// cloudinary.uploader.upload(
+//   "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" },
+//   function (error, result) {
+//     console.log(result);
+//   }
+// );
+
+
+export {uploadOnCloudinary}
